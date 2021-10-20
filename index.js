f92d0f1c7b012f9a6f9306d9e95dc602e9c3917f
Akindele <AKINDELEAYORINDE55@GMAIL.COM>
2021-10-20 21:12:24 +0200

    initial commit
    description

---
+++ index.js
@@ -0,0 +1,13 @@
+let open = document.getElementById("menu-btn")
+let nav = document.getElementById("nav")
+let exit = document.getElementById("exit-btn")
+
+
+open.addEventListener("click", ()=>{
+    nav.classList.add("open-nav")
+})
+
+
+exit.addEventListener("click", ()=>{
+    nav.classList.remove("open-nav")
+})
